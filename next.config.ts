cf25551e3c4a1541e82322c893ca6c511c020964
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-09-26 22:39:06 +0300

    chore: remove GH pages deploy option

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Export as static site
-  images: {
-    unoptimized: true, // Disable Next.js image optimization (not supported on GitHub Pages)
-  },
 };
 
 export default nextConfig;
